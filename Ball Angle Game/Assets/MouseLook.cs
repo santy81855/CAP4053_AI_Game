0006ceddc4112a42dbe787d12b5862eac93aa0d6
santy81855 <mynameissantiagogarcia@gmail.com>
2021-09-18 20:04:50 -0400

    added a cannon model and it can rotate along with the camera movement

---
+++ Ball Angle Game/Assets/MouseLook.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        mouseX = Mathf.Clamp(mouseX, -90f, 90f);
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+        
+        
+        //xRotation = mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        playerBody.Rotate(Vector3.up * mouseX);
+        playerBody.Rotate(Vector3.right * -mouseY);        
+        
+    }
+}
