093ef3716d2cd03a8f5d9bfaeec3f9e2f709712e
kyleolson812 <kyleolson812@gmail.com>
2021-09-26 17:48:02 -0400

    Implemented cannon mechanics onto cannon model

--- Ball Angle Game/Assets/Scripts/CannonController.cs
+++ Ball Angle Game/Assets/Scripts/CannonController.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    // Cannon Rotation Variables
-    public int speed;
-    public float friction;
-    public float lerpSpeed;
-    float xDegrees;
-    float yDegrees;
-    Quaternion fromRotation;
-    Quaternion toRotation;
-    private new Camera camera;
-
     // Cannon Firing Variables
     public GameObject cannonBall;
     Rigidbody cannonballRB;
@@ -21,22 +11,13 @@ public class CannonController : MonoBehaviour
     public GameObject explosion;
     public float firePower;
     public int powerMultiplier = 100;
-    // Start is called before the first frame update
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-    public float minimumY = -60F;
-    public float maximumY = 60F;
-    float rotationX = 0F;
-    float rotationY = 0F;
     public float mouseSensitivity = 100.0f;
-    public float clampAngle = 80.0f;
-    
+    private float rotY;
+    private float rotX;
 
-    private float rotY = 0.0f; // rotation around the up/y axis
-    private float rotX = 0.0f; // rotation around the right/x axis
     void Start()
     {
-        camera = Camera.main;
+
         firePower *= powerMultiplier;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.lockState = CursorLockMode.None;
@@ -56,42 +37,21 @@ public class CannonController : MonoBehaviour
         rotY += mouseX * mouseSensitivity * Time.deltaTime;
         rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-        rotY = Mathf.Clamp(rotY, -clampAngle, clampAngle);
+        // Up/Down
+        rotX = Mathf.Clamp(rotX, -75, 75);
 
+        // Left/Right
+        rotY = Mathf.Clamp(rotY, -25, 205);
 
         Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
         transform.rotation = localRotation;
-
-        //xDegrees -= Input.GetAxis("Mouse Y") * speed * friction;
-        //yDegrees += Input.GetAxis("Mouse X") * speed * friction;
-        //rotationX = ClampAngle(xDegrees, minimumX, maximumX);
-        //fromRotation = transform.rotation;
-
-        // Initial position of the cannon
-
-
-        //transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
-
-        //rotationY = ClampAngle(yDegrees, minimumY, maximumY);
-        //toRotation = Quaternion.Euler(rotationX, 90 + rotationY, 0);
-        //Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-        //Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
-
 
         if (Input.GetMouseButtonDown(0))
         {
             FireCannon();
         }
     }
-    public static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-         angle += 360F;
-        if (angle > 360F)
-         angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
-    }
+
     public void FireCannon()
     {
         GameObject cannonBallCopy = Instantiate(cannonBall, shotPos.position, transform.rotation) as GameObject;
