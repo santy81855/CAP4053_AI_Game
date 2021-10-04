208b24f422ebc224ce373dddabf639a643c33a58
JoelShaw0 <joelshaw@knights.ucf.edu>
2021-10-04 17:33:24 -0400

    Added Time Delay to Despawn
    Enemies can despawn after 3 seconds which can be changed in the unity editor. Currently the enemy still moves after being hit in the 3 second duration.

--- Ball Angle Game/Assets/Scripts/CannonBall.cs
+++ Ball Angle Game/Assets/Scripts/CannonBall.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class CannonBall : MonoBehaviour
 {
+    // Despawn time can be changed from editor
+    [SerializeField]
+    private float despawnTime = 3f;
+
+    // Detects if the cannon ball collides with an object that has the Is trigger option selected.
+    // Function has been modified to detect any object that has the Is trigger option selected in the unity editor.
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Capsule")
+        if (other.gameObject == true)//.name == "Capsule")
         {
             Debug.Log("Dead");
-            //gameObject.transform.Translate(90, 90, 90);
-            Destroy(other.gameObject);
+            StartCoroutine(deadTarget(other));
         }
+    }
+
+    // Delays the despawn of the enemy off 3 seconds intially. Currently the enemy still moves after being hit.
+    public IEnumerator deadTarget(Collider other)
+    {
+        yield return new WaitForSeconds(despawnTime);
+        Debug.Log("Waiting 3 seconds");
+        Destroy(other.gameObject);
     }
 }
