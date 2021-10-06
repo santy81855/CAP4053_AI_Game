98992b0ec59cafcfb8b9e9c5e562fd10b377d684
JoelShaw0 <joelshaw@knights.ucf.edu>
2021-10-06 17:15:59 -0400

    Enemies now Fly (Slide) Back
    Enemies fly back when they are hit however there is a bug with the shots. If an enemy is hit by multiple shots there is a nullpointexception. I think I know how to fix but I don't have tine today. Will fix tomorrow.

--- Ball Angle Game/Assets/Scripts/CannonBall.cs
+++ Ball Angle Game/Assets/Scripts/CannonBall.cs
@@ -7,14 +7,20 @@ public class CannonBall : MonoBehaviour
     // Despawn time can be changed from editor
     [SerializeField]
     private float despawnTime = 3f;
-
+    [SerializeField]
+    private int hits = 0;
     // Detects if the cannon ball collides with an object that has the Is trigger option selected.
     // Function has been modified to detect any object that has the Is trigger option selected in the unity editor.
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == true)//.name == "Capsule")
+        if (other.gameObject == null)
+        {
+            return;
+        }
+        if (hits == 0 && other.gameObject == true)//.name == "Capsule")
         {
             Debug.Log("Dead");
+            hits++;
             StartCoroutine(deadTarget(other));
         }
     }
