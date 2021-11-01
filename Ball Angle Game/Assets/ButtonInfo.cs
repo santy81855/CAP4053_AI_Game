82be18e7c9b14590009fc92645f3957a8bb7db47
kyleolson812 <kyleolson812@gmail.com>
2021-11-01 17:46:07 -0400

    Created base of the UIShop

--- Ball Angle Game/Assets/ButtonInfo.cs
+++ Ball Angle Game/Assets/ButtonInfo.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonInfo : MonoBehaviour
 {
     public int itemID;
-    public Text priceText;
-    public Text quantityText;
+    public TMP_Text priceText;
+    public TMP_Text quantityText;
     public GameObject shopManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        priceText.text = "Price: $" + shopManager.GetComponent<ShopManager>().shopItems[2, itemID].ToString();
+        quantityText.text = shopManager.GetComponent<ShopManager>().shopItems[3, itemID].ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
