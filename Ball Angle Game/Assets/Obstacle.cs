3e7e531551c64481c218cda48e28a1838a95cde6
Johan Sarmiento <johan.sarmiento99@knights.ucf.edu>
2021-11-19 21:41:14 -0500

    Obstacle

---
+++ Ball Angle Game/Assets/Obstacle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Obstacle : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    
+    private void Start()
+    {
+        agent.updateRotation = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                //Move our agent
+                agent.SetDestination(hit.point);
+            }
+        }
+
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+
+    }
+}
